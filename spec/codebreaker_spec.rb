893fbccb2a4b9981742e502f21d6925a5d4fef08
MorpheusFR <Creatorsk@gmail.com>
2018-06-12 21:05:23 +0300

    First commit in feature

--- spec/codebreaker_spec.rb
+++ spec/codebreaker_spec.rb
@@ -1,9 +1,13 @@
 RSpec.describe Codebreaker do
-  it "has a version number" do
-    expect(Codebreaker::VERSION).not_to be nil
-  end
+  # it "has a version number" do
+  #   expect(Codebreaker::VERSION).not_to be nil
+  # end
 
-  it "does something useful" do
-    expect(false).to eq(true)
+  # it "does something useful" do
+  #   expect(false).to eq(true)
+  # end
+
+  it 'saves secret string with numbers from 1 to 6' do
+    expect(game.instance_variable_get(:@secret_code)).to match(/^[1-6]{4}\z/)
   end
 end
