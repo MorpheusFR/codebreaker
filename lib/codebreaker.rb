2de4c03e8215fe6fe81e910376f424e8bd9efe1d
MorpheusFR <Creatorsk@gmail.com>
2018-06-20 16:41:11 +0300

    Done find_cows_and_bulls

--- lib/codebreaker.rb
+++ lib/codebreaker.rb
@@ -1,21 +1,24 @@
+#!/usr/bin/ruby
 require_relative 'codebreaker/version'
 # Comment to rubocop
 module Codebreaker
   # Comment to rubocop
   class Game
     def initialize
-      new_game
-      code_generator
-      code_view_with_hint
-      find_cows_and_bulls
-    end
-
-    def new_game
       @secret_code = code_generator
       @total_attempts = 6
       @hint = 4
       @hints_used = 0
       @input_code = '3214'
+      @result_of_an_attempt = find_cows_and_bulls
+      @result_of_the_game = 0
+    end
+
+    def new_game
+      code_generator
+      code_view_with_hint
+      find_cows_and_bulls
+      info
     end
 
     def code_generator
@@ -27,10 +30,27 @@ module Codebreaker
       @secret_code.slice(@hints_used, @secret_code.size).tr(@secret_code, '*'))
     end
 
+    def info
+      p @secret_code.chars
+      p @input_code.chars
+    end
+
     def find_cows_and_bulls
-      p _secret_code_array = @secret_code.chars
-      p input_code_array = @input_code.chars
-      input_code_array.each { |input_elem| p input_elem }
+      @result = ''
+      @input_code.chars.each do |elem|
+        @result << if @secret_code.slice(elem)
+          if @input_code.index(elem) == @secret_code.index(elem)
+            '+'
+          else
+            '-'
+          end
+        else
+          ' '
+        end
+      end
+      p @result
+      p @secret_code.chars
+      p @input_code.chars
     end
   end
 end
